d5d92cc8440dbfc98616a5ad79a43be756a4447c
Murilo Andrade <muriloap.info@gmail.com>
2022-05-11 08:05:39 -0300

    Adiciona response header para acesso público do Cors

--- server/dist/server.js
+++ server/dist/server.js
@@ -8,6 +8,10 @@ const cors_1 = __importDefault(require("cors"));
 const routes_1 = require("./routes");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    next();
+});
 app.use(express_1.default.json()); //importante ser antes das rotas
 app.use(routes_1.routes);
 app.listen(process.env.PORT || 3333, () => {
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,10 @@ import { routes } from "./routes";
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  next();
+})
 app.use(express.json()); //importante ser antes das rotas
 app.use(routes);
 
